82e2cbbdfe18ee106edd58384e3998e70a90dd97
kevdome3000 <kbassjr@gmail.com>
2023-12-01 14:01:58 -0800

    Extend KernelArguments with additional methods
    The KernelArguments class has been extended with new methods that allow for a more flexible management of arguments. This includes a method for initializing with an input string, updating the main input text, storing additional data, and printing out the processed data. The changes also include revisions to existing methods to align with these new functionalities.

--- dotnet/src/SemanticKernel.Abstractions/Functions/KernelArguments.cs
+++ dotnet/src/SemanticKernel.Abstractions/Functions/KernelArguments.cs
@@ -21,13 +21,56 @@ public sealed class KernelArguments : Dictionary<string, string>
     public const string InputParameterName = "input";
 
 
+    public KernelArguments(string input, PromptExecutionSettings? executionSettings = null) : this(executionSettings)
+    {
+        Set(InputParameterName, input);
+    }
+
+
     /// <summary>
     /// Initializes a new instance of the <see cref="KernelArguments"/> class with the specified AI request settings.
     /// </summary>
     /// <param name="executionSettings">The prompt execution settings.</param>
-    public KernelArguments(PromptExecutionSettings? executionSettings = null) : base(StringComparer.OrdinalIgnoreCase)
+    public KernelArguments(PromptExecutionSettings? executionSettings = null) : base(StringComparer.OrdinalIgnoreCase) => ExecutionSettings = executionSettings;
+
+
+    /// <summary>Gets the main input string.</summary>
+    /// <remarks>If the main input string was removed from the collection, an empty string will be returned.</remarks>
+    public string Input => TryGetValue(InputParameterName, out string? value) ? value : string.Empty;
+
+
+    /// <summary>
+    /// Updates the main input text with the new value after a function is complete.
+    /// </summary>
+    /// <param name="value">The new input value, for the next function in the pipeline, or as a result for the user
+    /// if the pipeline reached the end.</param>
+    /// <returns>The current instance</returns>
+    public KernelArguments Update(string? value)
     {
-        this.ExecutionSettings = executionSettings;
+        Set(InputParameterName, value);
+        return this;
+    }
+
+
+    /// <summary>
+    /// This method allows to store additional data in the context variables, e.g. variables needed by functions in the
+    /// pipeline. These "variables" are visible also to semantic functions using the "{{varName}}" syntax, allowing
+    /// to inject more information into prompt templates.
+    /// </summary>
+    /// <param name="name">Variable name</param>
+    /// <param name="value">Value to store. If the value is NULL the variable is deleted.</param>
+    public void Set(string name, string? value)
+    {
+        Verify.NotNullOrWhiteSpace(name);
+
+        if (value != null)
+        {
+            this[name] = value;
+        }
+        else
+        {
+            Remove(name);
+        }
     }
 
 
@@ -37,7 +80,7 @@ public sealed class KernelArguments : Dictionary<string, string>
     /// <param name="source">The <see cref="IDictionary{TKey, TValue}"/> whose elements are copied the new <see cref="KernelArguments"/>.</param>
     public KernelArguments(IDictionary<string, string> source) : this()
     {
-        foreach (var x in source) { this[x.Key] = x.Value; }
+        foreach (KeyValuePair<string, string> x in source) { this[x.Key] = x.Value; }
     }
 
 
@@ -45,10 +88,14 @@ public sealed class KernelArguments : Dictionary<string, string>
     /// Initializes a new instance of the <see cref="KernelArguments"/> class that contains AI request settings and elements copied from the specified <see cref="KernelArguments"/>
     /// </summary>
     /// <param name="other">The <see cref="KernelArguments"/> whose AI request setting and elements are copied to the new <see cref="KernelArguments"/>.</param>
-    public KernelArguments(KernelArguments other) : this(other as IDictionary<string, string>)
-    {
-        this.ExecutionSettings = other.ExecutionSettings;
-    }
+    public KernelArguments(KernelArguments other) : this(other as IDictionary<string, string>) => ExecutionSettings = other.ExecutionSettings;
+
+
+    /// <summary>
+    /// Print the processed input, aka the current data after any processing occurred.
+    /// </summary>
+    /// <returns>Processed input, aka result</returns>
+    public override string ToString() => Input;
 
 
     /// <summary>
