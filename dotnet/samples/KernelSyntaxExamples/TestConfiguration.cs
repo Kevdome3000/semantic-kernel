dfcd4d512623f8faa428e902c48441f48704a49c
Devis Lucato <dluc@users.noreply.github.com>
2023-12-15 05:38:14 -0800

    .Net: Quick fix for Azure DallE2 + 3 (#3932)
    ### Motivation and Context
    
    Some syntax errors, passing the API key as the model ID, or attempting
    to pass "dall-e3" model ID, leading to exceptions. DallE3 uses a
    different HTTP flow compared to Dalle2, so the existing code could not
    be reused. Azure AI SDK for .NET doesn't support DallE3 either.
    
    ### Description
    
    Refactor Azure Image generator to use Azure AI SDK for DallE2.
    Add explicit check for DallE3 which is not supported yet by Azure AI
    SDK.
    
    Future TODO:
    * Refactor image generation to use Azure AI SDK also for OpenAI
    * Update width/height checks, which are different for DallE3
    * Support Dall-E 3 for OpenAI Client Calls.
    
    ---------
    
    Co-authored-by: Mark Wallace <127216156+markwallace-microsoft@users.noreply.github.com>
    Co-authored-by: Roger Barreto <19890735+RogerBarreto@users.noreply.github.com>
    
    (cherry picked from commit 1b5aaede8c60d19e32d0794c5127b91ef14f0b5a)

--- dotnet/samples/KernelSyntaxExamples/TestConfiguration.cs
+++ dotnet/samples/KernelSyntaxExamples/TestConfiguration.cs
@@ -26,6 +26,7 @@ public sealed class TestConfiguration
 
     public static OpenAIConfig OpenAI => LoadSection<OpenAIConfig>();
     public static AzureOpenAIConfig AzureOpenAI => LoadSection<AzureOpenAIConfig>();
+    public static AzureOpenAIConfig AzureOpenAIImages => LoadSection<AzureOpenAIConfig>();
     public static AzureOpenAIEmbeddingsConfig AzureOpenAIEmbeddings => LoadSection<AzureOpenAIEmbeddingsConfig>();
     public static AzureAISearchConfig AzureAISearch => LoadSection<AzureAISearchConfig>();
     public static QdrantConfig Qdrant => LoadSection<QdrantConfig>();
@@ -78,9 +79,12 @@ public sealed class TestConfiguration
         public string ModelId { get; set; }
         public string ChatDeploymentName { get; set; }
         public string ChatModelId { get; set; }
+        public string ImageDeploymentName { get; set; }
         public string ImageModelId { get; set; }
+        public string ImageEndpoint { get; set; }
         public string Endpoint { get; set; }
         public string ApiKey { get; set; }
+        public string ImageApiKey { get; set; }
     }
 
 
