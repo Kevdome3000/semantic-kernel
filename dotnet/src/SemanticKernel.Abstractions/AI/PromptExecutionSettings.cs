82424a74e34f936ccd1cd344a3d4e80e231f18e8
kevdome3000 <kbassjr@gmail.com>
2024-02-21 10:53:19 -0800

    Add Json Serialization to PromptExecutionSettings
    The System.Text.Json.Serialization namespace was added to the PromptExecutionSettings.cs file located in SemanticKernel.Abstractions. This brings extended functionality to this class allowing it to work effectively with JSON formatted data.

--- dotnet/src/SemanticKernel.Abstractions/AI/PromptExecutionSettings.cs
+++ dotnet/src/SemanticKernel.Abstractions/AI/PromptExecutionSettings.cs
@@ -5,6 +5,7 @@ namespace Microsoft.SemanticKernel;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Text.Json.Serialization;
 using ChatCompletion;
 using TextGeneration;
 
